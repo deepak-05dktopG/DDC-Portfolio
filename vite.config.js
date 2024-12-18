993285774c261d0ce94a052dae6c386d9811f7cc
deepak-05dktopG <kumardeepak59422@gmail.com>
2024-12-18 22:03:31 +0530

    new commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // Replace with your GitHub repository name
+  base: 'DDC-Portfolio', // Replace with your GitHub repository name
 
 })
